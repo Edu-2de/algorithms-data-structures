992d37100630ea226fc9f41f3f0323422ba83a45
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-04-08 13:54:36 -0300

    corrige retorno da função pop e mensagem da pilha vazia; implementa ordenação na função sort_stack

--- stack/stack.py
+++ stack/stack.py
@@ -24,7 +24,7 @@ class Stack:
     def pop(self):
         if(self.isEmpty() == True):
             print("Nothin to remove!")
-            return 
+            return None
         temp_val = self.head
         self.head = self.head.prev
         return temp_val.data
@@ -33,7 +33,8 @@ class Stack:
         if(self.isEmpty() == False):
             return self.head.data
         else:
-            return "Stack is empty!"
+            print("Stack is empty!")
+            return None
   
 
     def __str__(self):
@@ -47,15 +48,35 @@ class Stack:
             count += 1
             actual = actual.prev
         return values
+
+def sort_stack(originalStack):
+    temp_stack = Stack() 
     
+    while originalStack.isEmpty() == False:
+        temp_val = originalStack.pop()
+
+        if temp_stack.isEmpty() == True:
+            temp_stack.push(temp_val)
+        else:
+            while temp_stack.isEmpty() == False and temp_stack.peek() > temp_val:
+                originalStack.push(temp_stack.pop())
+            temp_stack.push(temp_val)
+
+    while temp_stack.isEmpty() == False:
+        originalStack.push(temp_stack.pop())
+
+    print(f"\n================= Pilha temporaria: ==================")
+    print(temp_stack)
+
+   
 test = Stack()
-test.push(1)
-test.push(2)
 test.push(3)
-test.push(4)
+test.push(2)
+test.push(7)
+test.push(9)
 test.push(5)
 print(test)
-test.pop()
-test.pop()
-test.pop()
+
+sort_stack(test)
+print(f"\n================= Pilha Original: ==================")
 print(test)
